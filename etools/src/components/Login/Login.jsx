b51d07d357f32b892b4e654d1d830f41b26dd8e1
Yahya-Naeem <yahyanaeemofficial@gmail.com>
2023-04-15 23:02:21 +0500

    frontends connected

--- etools/src/components/Login/Login.jsx
+++ etools/src/components/Login/Login.jsx
@@ -1,53 +1,57 @@
-import React, { Component } from 'react';
+import React, { Component ,useContext, useState} from 'react';
 import axios from 'axios';
-
-class User extends Component {
-  state = {
-    username: null,
-    password: null,
-    error: false
-  }
-
-  handleChange = (e) => {
-    this.setState({
-      [e.target.id]: e.target.value
-    });
-  }
-
-  handleSubmit = (e) => {
-    e.preventDefault();
-    const { username, password } = this.state;
-    if (username && password) {
-      axios.post('filepath', { username, password })
-        .then(response => {
-          /* set state true to show user dashboard*/
-        })
-        .catch(error => {
-          /*show error*/
-          this.setState({ error: true });
-        });
+import UserContext from '../UserContext';
+const User =() => {
+  /*Use context to access app.js functions */
+  const { handleLogin } = useContext(UserContext);
+    /**Variable in Login function  */
+    const [username,setusername] = useState(null)
+    const [password,setpassword] = useState(null)
+    const [error,seterror] = useState(false)
+    /*Sending data to backend */
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      handleLogin();
+      /*if (username && password) {
+        axios.post('filepath', { username, password })
+          .then(response => {
+            alert('Logged In Successfully')
+            handleLogIn();
+            /* set state true to show user dashboard*/
+       /*   })
+          .catch(error => {
+            /*show error*/
+       /*     alert(error)
+            seterror(true)
+          });
+      }*/
     }
-  }
-
-  render() {
-    const { error } = this.state;
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <div>
-          <label htmlFor="username">Username</label>
-          <input type="text" id="username" onChange={this.handleChange} />
-        </div>
-        <div>
-          <label htmlFor="password">Password</label>
-          <input type="password" id="password" onChange={this.handleChange} />
-        </div>
-        {error &&
-          <p> Invalid Login Or SignUp </p>
-        }
-        <button type="submit">Submit</button>
-      </form>
-    );
-  }
+    /*Handle functions  */
+    const handleusername = (e) =>{
+      setusername(e.target.value);
+    }
+    const handlepassword= (e) =>{
+      setpassword(e.target.value);
+    }
+    /*Rendering Body*/
+  return(
+    <div>
+    <form onSubmit={handleSubmit}>
+    <div>
+      <label htmlFor="username">Username</label>
+      <input type="text" id="username" value={username} onChange={handleusername} />
+    </div>
+    <div>
+      <label htmlFor="password">Password</label>
+      <input type="password" id="password" value={password} onChange={handlepassword} />
+    </div>
+    {error &&
+      <p> Invalid Login Or SignUp </p>
+    }
+    <button type="submit">Submit</button>
+  </form>
+  </div>
+  )
 }
 
 export default User;
