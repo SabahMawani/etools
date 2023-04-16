f87721a5be432c17acd9c167fc32ae5fdc198ab6
SabahMawani <sabahmawani@gmail.com>
2023-04-16 17:52:27 +0500

    profile updated

--- etools/src/components/Profile/Profile.jsx
+++ etools/src/components/Profile/Profile.jsx
@@ -1,9 +1,11 @@
 import React, { useState, useEffect, useContext } from 'react';
 import axios from 'axios';
+import './profile.css';
+import pImg from './../../assets/profile.jpg';
 import UserContext from '../UserContext';
 function Profile(){
     const {handleLogout} = useContext(UserContext)
-    const [username,setusername] = useState('')
+    const [username,setusername] = useState('unknown')
     const [userid,setuserid] = useState('123')
     const [email,setemail] = useState('@xyz.com')
     const [gender,setgender] = useState('-')
@@ -29,15 +31,22 @@ function Profile(){
         handleLogout();
     }
     return(
-        <div>
-            <h2>Welcome to E-tools {username}</h2>
-            <p className=''>User ID : {userid}</p>
-            <p className=''>Email : {email}</p>
-            <p className=''>Gender : {gender}</p>
-            <p className=''>Quiz High Score : {quiz}</p>
-            <p className=''>Typing High Score : {type}</p>
-            <div className='tobtn-group'>
-            <button onClick={handleclick}>Logout</button>
+        <div className='profile-background'>
+            <img className='profile-bg-img' src={pImg} alt="Background Image" />
+            <div className='profile-container'>
+                <div className='heading-container'>
+                    <h2 className='profile-heading'>Welcome to E-tools {username}</h2>
+                    <div className='pro-container'>
+                        <p className=''><b>User ID : </b>{userid}</p>
+                        <p className=''><b>Email : </b>{email}</p>
+                        <p className=''><b>Gender : </b>{gender}</p>
+                        <p className=''><b>Quiz High Score : </b>{quiz}</p>
+                        <p className=''><b>Typing High Score : </b>{type}</p>
+                    </div>
+                    <div className='tobtn-group'>
+                        <button onClick={handleclick}>Logout</button>
+                    </div>
+                </div>
             </div>
         </div>
     );
