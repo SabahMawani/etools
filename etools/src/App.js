58a121dea8734312effaaf20820aaed2db91b29e
SabahMawani <sabahmawani@gmail.com>
2023-05-01 17:21:18 +0500

    update

--- etools/src/App.js
+++ etools/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState,  useEffect } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Routes ,vigate } from 'react-router-dom';
-import 'materialize-css/dist/css/materialize.min.css';
+//import 'materialize-css/dist/css/materialize.min.css';
 import UserContext from './components/UserContext';
 
 import {
